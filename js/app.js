619d22ced453858a40a6546e6695f6fb36e7c62f
Alexander Shubin <mizerai@mail.ru>
2022-07-06 13:44:39 +0700

    add form+footer

--- js/app.js
+++ js/app.js
@@ -7,3 +7,10 @@ $(function () {
 		autoplayspeed: 2000,
 	});
 });
+
+$(".menu, .bike__column").on("click", "a", function (event) {
+	event.preventDefault();
+	var id = $(this).attr("href"),
+		top = $(id).offset().top;
+	$("body,html").animate({ scrollTop: top }, 1000);
+});
